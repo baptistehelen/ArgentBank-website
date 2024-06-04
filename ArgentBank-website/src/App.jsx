8f032c5a7825ffd4f4e76c38da4e3a5fde613607
baptistehelen <148351322+baptistehelen@users.noreply.github.com>
2024-06-04 14:54:21 +0200

    Implement landing page 1#

--- ArgentBank-website/src/App.jsx
+++ ArgentBank-website/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { User } from "./components/User";
+import { Home } from "./components/Home";
+import { Signin } from "./components/Signin";
+import { Navbar } from "./layout/Navbar";
+import { Footer } from "./layout/Footer";
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/sign-in" element={<Signin />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- ArgentBank-website/vite.config.js
+++ ArgentBank-website/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Liste d'autorisation pour les chemins d'accès du serveur de développement
+    // Inclut les chemins relatifs au répertoire de travail actuel et au répertoire public
+    fs: {
+      // Ajoute le répertoire "designs" à la liste d'autorisation
+      allow: ['.', './public', './../designs']
+    }
+  }
 })
